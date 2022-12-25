6fbfcafadf167f8599b99fa959c5777563073c14
petitbonney <petitbonney@gmail.com>
2022-12-25 16:14:54 +0100

    Use unofficial API, creating an official Facebook App is boring

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 import dotenv from "dotenv";
-import InstagramAPI from "./instagram.js";
+import Instagram from "./instagram.js";
 
 dotenv.config();
 
-const instagram = new InstagramAPI();
+const run = async () => {
+  const ig = new Instagram();
+  const me = await ig.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
 
-const echo = (clientId, text) => {
-  instagram.sendMessage(clientId, text);
+  const loop = setInterval(async () => {
+    await ig.approveAll();
+    const inbox = await ig.getInbox();
+    for (const thread of inbox) {
+      const messages = await ig.getNotSeenMessages(thread, me.pk);
+      for (const msg of messages) {
+        console.log(`${thread.users[0].username} says: ${msg.text}`);
+        await thread.markItemSeen(msg.item_id);
+        await thread.broadcastText(msg.text);
+      }
+    }
+  }, 5000);
+
+  console.log("Running...");
 };
 
-instagram.onMessage(echo);
-
-instagram.run();
+run();
--- instagram.js
+++ instagram.js
@@ -1,128 +1,61 @@
-import express from "express";
-import parser from "body-parser";
-import crypto from "crypto";
-import fetch from "node-fetch";
+import { IgApiClient } from "instagram-private-api";
 
-// Verify that the callback came from Facebook.
-function verifyRequestSignature(req, res, buf) {
-  var signature = req.headers["x-hub-signature"];
-
-  if (!signature) {
-    console.warn(`Couldn't find "x-hub-signature" in headers.`);
-  } else {
-    var elements = signature.split("=");
-    var signatureHash = elements[1];
-    var expectedHash = crypto
-      .createHmac("sha1", process.env.APP_SECRET)
-      .update(buf)
-      .digest("hex");
-    if (signatureHash != expectedHash) {
-      throw new Error("Couldn't validate the request signature.");
+function zipThreads(records, items) {
+  const threads = [];
+  for (const r of records) {
+    let thread = r;
+    for (const i of items) {
+      if (i.thread_id === r.threadId) {
+        thread = Object.assign(r, i);
+      }
     }
+    threads.push(thread);
+  }
+  return threads;
+}
+
+class Instagram {
+  ig;
+
+  constructor() {
+    this.ig = new IgApiClient();
+  }
+
+  async login(username, password) {
+    this.ig.state.generateDevice(username);
+    await this.ig.simulate.preLoginFlow();
+    const me = await this.ig.account.login(username, password);
+    // process.nextTick(async () => await this.ig.simulate.postLoginFlow());
+    return me;
+  }
+
+  async getInbox() {
+    const inbox = await this.ig.feed.directInbox();
+    const records = await inbox.records();
+    const items = await inbox.items();
+    return zipThreads(records, items);
+  }
+
+  async getPending() {
+    const inbox = await this.ig.feed.directPending();
+    const items = await inbox.items();
+    const records = await inbox.records();
+    return zipThreads(items, records);
+  }
+
+  async approveAll() {
+    const pending = await this.getPending();
+    return await this.ig.directThread.approveMultiple(pending.keys());
+  }
+
+  async getParticipantUsernames(thread) {
+    return thread.users.map((x) => x.username);
+  }
+
+  async getNotSeenMessages(thread, pk) {
+    const lastSeenAt = parseInt(thread.last_seen_at[pk].timestamp);
+    return thread.items.filter((x) => parseInt(x.timestamp) > lastSeenAt);
   }
 }
 
-export default class InstagramAPI {
-  client;
-  port;
-  callback;
-
-  constructor(port = 3000) {
-    this.port = port;
-    this.callback = null;
-    this.client = express();
-    this.init();
-  }
-
-  init() {
-    // Parse application/x-www-form-urlencoded
-    this.client.use(
-      parser.urlencoded({
-        extended: true,
-      })
-    );
-
-    // Parse application/json. Verify that callback came from Facebook
-    this.client.use(parser.json({ verify: verifyRequestSignature }));
-
-    // Add support for GET requests to our webhook
-    this.client.get("/webhook", (req, res) => {
-      // Parse the query params
-      const mode = req.query["hub.mode"];
-      const token = req.query["hub.verify_token"];
-      const challenge = req.query["hub.challenge"];
-
-      // Check if a token and mode is in the query string of the request
-      if (mode && token) {
-        // Check the mode and token sent is correct
-        if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
-          // Respond with the challenge token from the request
-          console.log("WEBHOOK_VERIFIED");
-          res.status(200).send(challenge);
-        } else {
-          // Respond with '403 Forbidden' if verify tokens do not match
-          res.sendStatus(403);
-        }
-      }
-    });
-
-    // Create the endpoint for your webhook
-    this.client.post("/webhook", (req, res) => {
-      let body = req.body;
-      console.log("EVENT_RECEIVED");
-
-      // Check if this is an event from a page subscription
-      if (body.object === "instagram") {
-        const content = body.entry[0].messaging[0];
-        const clientId = content.sender.id;
-        const text = content.message.text;
-        const isEcho = content.message.is_echo;
-        if (!isEcho) {
-          console.log(JSON.stringify(body, null, 2));
-          if (this.callback) {
-            this.callback(clientId, text);
-          }
-        }
-        // Returns a '200 OK' response to all requests
-        res.status(200).send("EVENT_RECEIVED");
-      } else {
-        // Return a '404 Not Found' if event is not from a page subscription
-        res.sendStatus(404);
-      }
-    });
-  }
-
-  run() {
-    this.client.listen(process.env.PORT, () => {
-      console.log(`The app is listening on port ${this.port}`);
-    });
-  }
-
-  onMessage(callback) {
-    this.callback = callback;
-  }
-
-  async sendMessage(clientId, text) {
-    const url = `${process.env.FB_API_URL}/${process.env.FB_API_VERSION}/${process.env.PAGE_ID}/messages`;
-    const data = {
-      recipient: { id: clientId },
-      message: { text: text },
-      access_token: process.env.PAGE_ACCESS_TOKEN,
-    };
-    return await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((res) => {
-        if (res.status !== 200) {
-          console.log(res);
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }
-}
+export default Instagram;
