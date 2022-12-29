11b01aa02e851e4f09286d532d9c4ea7eae00a14
petitbonney <petitbonney@gmail.com>
2022-12-29 19:41:48 +0100

    setTimeout instead of setInterval

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { ChatGPTAPIBrowser } from "chatgpt";
+import { ChatGPTAPIBrowser, ChatGPTAPI } from "chatgpt";
 import dotenv from "dotenv";
 import Context from "./context.js";
 import Instagram from "./instagram.js";
@@ -10,6 +10,7 @@ const run = async () => {
     email: process.env.OPENAI_EMAIL,
     password: process.env.OPENAI_PASSWORD,
     isGoogleLogin: true,
+    executablePath: process.env.CHROME_PATH,
   });
   await chatgpt.initSession();
 
@@ -20,24 +21,41 @@ const run = async () => {
     await ig.approveAll();
     const inbox = await ig.getInbox();
     for (const thread of inbox) {
+      // Extract info from thread
       const threadId = thread.threadId;
+      const users = {};
+      for (const u of thread.users) {
+        users[u.pk] = u.username;
+      }
+      // Extract not seen messages by me
       const messages = await ig.getNotSeenMessages(thread, me.pk);
       for (const msg of messages) {
+        // Get thread context
         const context = Context.getContext(threadId);
+        // Get username from key
+        const username = users[msg.user_id];
+        console.log(`${username}: "${msg.text}"`);
+        // Mark as seen
         await thread.markItemSeen(msg.item_id);
+        // Send message to ChatGPT
         const result = await chatgpt.sendMessage(msg.text, context);
         for (const response of result.response.split("\n\n")) {
+          // Broadcast ChatGPT response to thread
           await thread.broadcastText(response);
         }
+        console.log(`ChatGPT replied to ${username}.`);
+        // Update context
         Context.setContext(threadId, {
           conversationId: result.conversationId,
           parentMessageId: result.messageId,
         });
       }
     }
+    setTimeout(loop, 5000);
   };
 
-  setInterval(loop, 5000);
+  // setInterval(loop, 5000);
+  loop();
 
   console.log("Running...");
 };
